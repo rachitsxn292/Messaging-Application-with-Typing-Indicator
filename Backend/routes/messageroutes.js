431ec8c457a2841948f6421cc59c2024d2711230
rachitsxn292 <rachit.saxena@sjsu.edu>
2019-12-14 19:02:39 -0800

    changes - rachit

--- Backend/routes/messageroutes.js
+++ Backend/routes/messageroutes.js
@@ -1,6 +1,7 @@
 var MongoClient = require("mongodb").MongoClient;
 var assert = require("assert");
-const express = require("express");
+var cookie= require("react-cookies");
+const express = require('express');
 const app = express();
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
@@ -16,30 +17,26 @@ const mongoose = require("mongoose");
 const chatroom = require("../schema/chatroom_model");
 const message = require("../schema/message_model");
 
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-io.on("connection", function(socket) {
-  console.log("a user connected");
-  socket.on("disconnect", function() {
-    console.log("User Disconnected");
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('User Disconnected');
   });
-  socket.on("Hello", function(msg) {
-    console.log("message: " + msg);
-    socket.broadcast.emit("typ_msg", msg + " typing...");
+  socket.on('Hello', function(msg){
+    console.log('message: ' + msg);
+    socket.broadcast.emit('typ_msg', msg +' typing...');
   });
-  socket.on("keyUp", function(msg) {
-    socket.broadcast.emit("keyUp_msg", "");
-  });
+  socket.on('keyUp', function(msg){
+    socket.broadcast.emit('keyUp_msg', '');
+  })
 });
 io.listen(8000);
 
 exports.getMessageList = function(req, res) {
   console.log("Print id  " + req.query.roomId);
-  // var data = {
-  //   // buyerId: req.query.buyerId,
-  //   // orderId: req.query.orderId
-  //   roomId: req.query.roomId
-  // };
+ 
 
   message.find({ roomId: req.query.roomId }, function(err, doc) {
     if (doc) {
@@ -56,7 +53,8 @@ exports.getMessageList = function(req, res) {
 exports.sendMessage = function(req, res) {
   var content = req.query.messages;
   var roomId = req.query.roomId;
-  console.log("send Message req section", content);
+  var emailid= req.query.email_id;
+  console.log("send Message req section",content);
 
   var today = new Date();
 
@@ -64,16 +62,17 @@ exports.sendMessage = function(req, res) {
     _id: new mongoose.Types.ObjectId(),
     message: content,
     roomId: roomId,
+    userId: emailid,
     created: today
   });
   message1
     .save()
     .then(resul => {
-      console.log("send Message Saved", resul);
-      console.log("message enrolled");
+      console.log("send Message Saved",resul);
+
       res.send({
         code: 200,
-        success: "User registered sucessfully"
+        success: "User registered sucessfully",
       });
     })
     .catch(err => {
